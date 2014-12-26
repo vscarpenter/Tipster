1c1d102e111377af7c0f32903aebe665250f95c6
Vinny Carpenter <vscarpenter@gmail.com>
2014-12-26 08:45:38 -0600

    Implemented v1.0
    Implemented the tip calculator based on Shrikar’s blog entry

---
+++ Tipster/Tipster/SettingsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SettingsViewController.swift
+//  Tipster
+//
+//  Created by Vinny Carpenter on 12/26/14.
+//  Copyright (c) 2014 Vinny Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var tipControl: UISegmentedControl!
+    let tips = [18, 20, 25]
+    var userDefaults : NSUserDefaults?
+    let defaultTip: Double?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selectCorrectSegment()
+        tipControl.layer.cornerRadius = 5.0
+        tipControl.layer.masksToBounds = true
+        tipControl.tintColor = UIColor(red: 92.0/255, green: 69.0/255, blue: 133.0/255, alpha: 0.8)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.view.alpha = 0.6
+        UIView.animateWithDuration(1.0, animations: { () -> Void in
+            self.view.alpha = 1.0
+        })
+      selectCorrectSegment()
+        
+    }
+    
+    func selectCorrectSegment() {
+        userDefaults = NSUserDefaults.standardUserDefaults()
+        let tipVal = userDefaults?.integerForKey("defaultTip")
+        tipControl.selectedSegmentIndex = 0
+        for (index,tmp) in enumerate(tips) {
+            if(tipVal == tmp){
+                tipControl.selectedSegmentIndex = index
+            }
+        }
+        let currentTip = tips[tipControl.selectedSegmentIndex]
+        self.tipLabel.text = "Default Tip is \(currentTip)%"
+    }
+    
+    @IBAction func defaultTipChanged(sender: AnyObject) {
+        let currentTip = tips[tipControl.selectedSegmentIndex]
+        self.tipLabel.text = "Default Tip is \(currentTip)%"
+        userDefaults?.setInteger(currentTip, forKey: "defaultTip")
+        userDefaults?.synchronize()
+    }
+}
---
+++ Tipster/Tipster/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Vinny Carpenter on 12/26/14.
+//  Copyright (c) 2014 Vinny Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var tipControl: UISegmentedControl!
+    
+    @IBOutlet var amountTextField: UITextField!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var tipLabel: UILabel!
+    
+    var userDefaults : NSUserDefaults?
+    let tips = [ 18, 20, 25]
+    var defaultTip : Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tipLabel.text = "$0.00"
+        self.totalLabel.text = "$0.00"
+        
+        tipControl.layer.cornerRadius = 5.0
+        tipControl.layer.masksToBounds = true
+        tipControl.tintColor = UIColor(red: 92.0/255, green: 69.0/255, blue: 133.0/255, alpha: 0.8)
+        tipControl.selectedSegmentIndex = 0
+        
+        userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        defaultTip = userDefaults?.integerForKey("defaultTip")
+        
+        for (index,tmp) in enumerate(tips) {
+            if(defaultTip == tmp){
+                tipControl.selectedSegmentIndex = index
+            }
+        }
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        defaultTip = userDefaults?.integerForKey("defaultTip")
+        
+        for (index,tmp) in enumerate(tips) {
+            if(defaultTip == tmp){
+                tipControl.selectedSegmentIndex = index
+            }
+        }
+        billAmtEntered(self)
+    }
+    
+    @IBAction func billAmtEntered(sender: AnyObject) {
+        calculateTipAndTotal()
+    }
+    
+    @IBAction func tipControlChange(sender: AnyObject) {
+        calculateTipAndTotal()
+    }
+    
+    func calculateTipAndTotal() {
+        let bill = (amountTextField.text as NSString).doubleValue
+        let total = bill + bill * (Double(tips[tipControl.selectedSegmentIndex])/100)
+        totalLabel.text = NSString(format: "$%.2f", total)
+        tipLabel.text = NSString(format: "$%.2f", bill * Double(tips[tipControl.selectedSegmentIndex])/100)
+        
+    }
+}
+
